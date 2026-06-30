735efdd1ae170d1e29b94ece9b421e1c886d2f20
Dev-AuraHub <anitagonzalezdavis750519@gmail.com>
2026-06-30 08:03:02 +0700

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,93 @@
-// constants.js
-module.exports = {};
+module.exports = {
+    RARITIES: {
+        COMMON: 'Common',
+        UNCOMMON: 'Uncommon',
+        RARE: 'Rare',
+        EPIC: 'Epic',
+        LEGENDARY: 'Legendary',
+        MYTHIC: 'Mythic',
+        ANCIENT: 'Ancient'
+    },
+
+    RARITY_WEIGHTS: {
+        'Common': 40,
+        'Uncommon': 30,
+        'Rare': 15,
+        'Epic': 8,
+        'Legendary': 4,
+        'Mythic': 2,
+        'Ancient': 1
+    },
+
+    RARITY_COLORS: {
+        'Common': '#808080',
+        'Uncommon': '#00FF00',
+        'Rare': '#0066FF',
+        'Epic': '#9900FF',
+        'Legendary': '#FF8C00',
+        'Mythic': '#FF0000',
+        'Ancient': '#FFD700'
+    },
+
+    EQUIPMENT_SLOTS: {
+        WEAPON: 'weapon',
+        ARMOR: 'armor',
+        HELMET: 'helmet',
+        BOOTS: 'boots',
+        GLOVES: 'gloves',
+        RING: 'ring',
+        NECKLACE: 'necklace'
+    },
+
+    BATTLE_TYPES: {
+        PVE: 'pve',
+        PVP: 'pvp',
+        BOSS: 'boss',
+        DUNGEON: 'dungeon',
+        RAID: 'raid'
+    },
+
+    GAMBLING_GAMES: {
+        COINFLIP: 'coinflip',
+        DICE: 'dice',
+        SLOTS: 'slots',
+        ROULETTE: 'roulette',
+        BLACKJACK: 'blackjack',
+        LOTTERY: 'lottery'
+    },
+
+    ADVENTURE_AREAS: {
+        FOREST: 'forest',
+        CAVE: 'cave',
+        DESERT: 'desert',
+        JUNGLE: 'jungle',
+        MOUNTAIN: 'mountain'
+    },
+
+    PET_RARITIES: {
+        COMMON: 'Common',
+        UNCOMMON: 'Uncommon',
+        RARE: 'Rare',
+        EPIC: 'Epic',
+        LEGENDARY: 'Legendary'
+    },
+
+    COOLDOWN_TIMES: {
+        DAILY: 86400000,
+        WEEKLY: 604800000,
+        MONTHLY: 2592000000,
+        WORK: 60000,
+        BEG: 30000,
+        CRIME: 120000,
+        ROB: 300000,
+        FISH: 60000,
+        MINE: 60000,
+        HUNT: 90000,
+        EXPLORE: 120000,
+        BATTLE: 10000,
+        BOSS: 300000,
+        DUNGEON: 600000,
+        GAMBLE: 5000,
+        PET: 10000
+    }
+};
